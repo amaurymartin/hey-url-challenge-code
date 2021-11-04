0e33efad290d411b922872d5b0556452148536e5
Amaury Martin <amaury.conde@hotmail.com>
2021-11-04 16:27:18 -0300

    Add scopes to click model

--- app/models/click.rb
+++ app/models/click.rb
@@ -5,4 +5,13 @@ class Click < ApplicationRecord
 
   validates :browser, presence: true
   validates :platform, presence: true
+
+  scope :on_current_month, lambda {
+    where(
+      created_at: Time.current.beginning_of_month..Time.current.end_of_month
+    )
+  }
+  scope :count_daily_clicks, -> { group('date(created_at)').count }
+  scope :count_by_browser, -> { group(:browser).count }
+  scope :count_by_platform, -> { group(:platform).count }
 end
--- spec/models/click_spec.rb
+++ spec/models/click_spec.rb
@@ -28,4 +28,66 @@ RSpec.describe Click, type: :model do
       it { is_expected.to be_invalid }
     end
   end
+
+  describe '.on_current_month' do
+    subject(:on_current_month) { Click.on_current_month }
+
+    before do
+      travel_to(1.month.ago) { create_list(:click, 2) }
+      create_list(:click, 3)
+    end
+
+    it { expect(on_current_month.count).to eq 3 }
+  end
+
+  describe '.count_daily_clicks' do
+    subject(:count_daily_clicks) { Click.count_daily_clicks }
+
+    before do
+      travel_to(2.day.ago) { create_list(:click, 1) }
+      travel_to(1.day.ago) { create_list(:click, 2) }
+      create_list(:click, 3)
+    end
+
+    it :aggregate_failures do
+      expect(count_daily_clicks.keys).to include(
+        2.day.ago.to_date, 1.day.ago.to_date, Date.today
+      )
+      expect(count_daily_clicks.values).to include(1, 2, 3)
+    end
+  end
+
+  describe '.count_by_browser' do
+    subject(:count_by_browser) { Click.count_by_browser }
+
+    let(:browsers) { %w[Chrome Firefox] }
+
+    before do
+      browsers.each_with_index do |browser, i|
+        create_list(:click, i + 1, browser: browser)
+      end
+    end
+
+    it :aggregate_failures do
+      expect(count_by_browser.keys).to include(*browsers)
+      expect(count_by_browser.values).to include(1, 2)
+    end
+  end
+
+  describe '.count_by_platform' do
+    subject(:count_by_platform) { Click.count_by_platform }
+
+    let(:platforms) { %w[Linux Windows] }
+
+    before do
+      platforms.each_with_index do |platform, i|
+        create_list(:click, i + 1, platform: platform)
+      end
+    end
+
+    it :aggregate_failures do
+      expect(count_by_platform.keys).to include(*platforms)
+      expect(count_by_platform.values).to include(1, 2)
+    end
+  end
 end
